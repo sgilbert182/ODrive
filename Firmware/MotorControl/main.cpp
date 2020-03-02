1abc382f741e27bf66b31bcbab0cf5830c285418
sgilbert182 <45004203+sgilbert182@users.noreply.github.com>
2020-03-02 11:45:33 +0000

    Moved from GPIO.c

--- Firmware/MotorControl/main.cpp
+++ Firmware/MotorControl/main.cpp
@@ -168,6 +168,30 @@ void vApplicationIdleHook(void) {
 }
 }
 
+
+// @brief Puts the GPIO's 1 and 2 into UART mode.
+// This will disable any interrupt subscribers of these GPIOs.
+void SetGPIO12toUART() {
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  // make sure nothing is hogging the GPIO's
+  SubscribeEXTI.unsubscribe(GPIO_1_GPIO_Port, GPIO_1_Pin);
+  SubscribeEXTI.unsubscribe(GPIO_2_GPIO_Port, GPIO_2_Pin);
+
+  GPIO_InitStruct.Pin = GPIO_1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+  HAL_GPIO_Init(GPIO_1_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = GPIO_2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+  HAL_GPIO_Init(GPIO_2_GPIO_Port, &GPIO_InitStruct);
+}
 int odrive_main(void) {
 
 #if HW_VERSION_MAJOR == 3 && HW_VERSION_MINOR >= 3
